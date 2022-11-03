17a6a983bf6c5f0ff5c54f7d5b7c82e78f63b8ee
John Dahlberg <jocd@netcompany.com>
2022-11-03 20:22:06 +0100

    olle text changes

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,15 @@ import Navbar from "../components/navbar";
 
 export default function Home() {
   function handleClick(brodet) {
-    if (brodet) {
+    if (brodet == "Brodet") {
       window.location =
         "mailto:johndahlberg@hotmail.com ?cc=johan@calgaro.se &subject=Vi vill ha Brödet såklart &body=Hej, vi är intresserade av ert grymma live band, här är mina kontaktuppgifter:";
-    } else {
+    } else if (brodet == "JohnOlle") {
       window.location =
         "mailto:johndahlberg@hotmail.com ?subject=John Olle &body=Hej, vi är intresserade av er grymma duo, hör gärna av er, här är mina kontaktuppgifter: ";
+    } else if (brodet == "Oggebomben") {
+      window.location =
+        "mailto:johndahlberg@hotmail.com ?subject=Oggebomben &body=Hej, vi är intresserade av er grymma DJ, här är mina kontaktuppgifter: ";
     }
   }
 
@@ -29,33 +32,34 @@ export default function Home() {
           flexDirection: "column",
           justifyContent: "space-evenly",
           alignItems: "center",
-          padding: "1em",
         }}
       >
         <div className="textContainer">
-          <p style={{ color: "rgb(216, 158, 81)" }}>
-            Brödet har något för allas stilar, önskas det party har vi ett fullt
-            live band som kan dra igång festen. Där spelas allt från angels med
-            Robbie williams till DU HAST.
+          <p>
+            <b>BRÖDET</b> är bandet med stort B - ett fullt 6-mannaliveband som
+            drar igång festen med ösiga blandade hits som ingen kan stå emot.
+            Boka oss till ert evenemang för en säker succékväll!
           </p>
-          <p style={{ color: "rgb(216, 158, 81)" }}>
-            Önskas det istället lite lugnare musik som kan dra igång
-            företagseventet eller öka stämningen på minglet så har vi även den
-            magiska duon John o Olle
+          <p>
+            Önskas istället lite lugnare musik som kan dra igång företagseventet
+            eller öka stämningen på minglet så har vi den magiska duon{" "}
+            <b>JOHN OCH OLLE</b>.
           </p>
-          <p style={{ color: "rgb(216, 158, 81)" }}>
-            Klicka på bilderna nedan för intresseanmälan!
+          <p>
+            För er som helst skippar middagen och åker direkt till den tyska
+            ravescenen kan ni boka <b>OGGEBOMBEN</b> som förgyller er natt.
           </p>
+          <p>Klicka på bilderna nedan för intresseanmälan!</p>
         </div>
         <div className="imageContainer">
-          <div className="image" onClick={() => handleClick(true)}>
+          <div className="image" onClick={() => handleClick("Brodet")}>
             <img src="/images/brodet.jpg"></img>
             <div className="image__overlay">
               <div className="image__title">Brödet</div>
               <p className="p.image__description">klicka här för bokning</p>
             </div>
           </div>
-          <div className="image" onClick={() => handleClick(false)}>
+          <div className="image" onClick={() => handleClick("JohnOlle")}>
             <img
               src="/images/johnolle.png"
               style={{ objectPosition: "top" }}
@@ -65,7 +69,18 @@ export default function Home() {
               <p className="p.image__description">klicka här för bokning</p>
             </div>
           </div>
+          <div className="image" onClick={() => handleClick("Oggebomben")}>
+            <img
+              src="/images/oggebomben.jpg"
+              style={{ objectPosition: "top" }}
+            ></img>
+            <div className="image__overlay">
+              <div className="image__title">Oggebomben</div>
+              <p className="p.image__description">klicka här för bokning</p>
+            </div>
+          </div>
         </div>
+        <div className="footer">johndahlberg@hotmail.com</div>
       </div>
     </div>
   );
